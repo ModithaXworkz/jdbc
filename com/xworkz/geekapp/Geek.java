cf2f6c28ed51e8f5536e34e5baaac52e183da9f8
ModithaXworkz <moditha5.xworkz@gmail.com>
2022-01-05 08:05:05 +0530

    apache

---
+++ com/xworkz/geekapp/Geek.java
@@ -0,0 +1,21 @@
+package com.xworkz.geekapp;
+import java.util.Scanner;
+public class Geek{
+static int[] num=new int[4];
+
+public static void main(String a[]){
+Scanner sc =new Scanner(System.in);
+for(int i=0;i<num.length;i++){
+ num[i]=sc.nextInt();
+}
+getNumbers(num);
+}
+public static int[] getNumbers(int[] num){
+for(int i=0;i<num.length;i++){
+ int number=num[i];
+ System.out.println(number);
+}
+return num;
+}
+}
+
